174d975c453f24a14a32d760639c02cb965d4f3e
samuele <samuelealbani@gmail.com>
2024-07-22 08:21:54 +0100

    create starting scripts

---
+++ start_client.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Start the server
+print("Starting the server (in 20 seconds)...")
+sleep 20
+# cd to the directory where the server is located
+# cd /path/to/server
+# Start the server
+python3 client.py
---
+++ start_server.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Start the server
+print("Starting the server (in 10 seconds)...")
+sleep 10
+# cd to the directory where the server is located
+# cd /path/to/server
+# Start the server
+python3 server.py
