10d0e2153dc2dcf4c9046f1fd5e44a48fb481dba
samuele <samuelealbani@gmail.com>
2024-07-22 08:29:38 +0100

    add venv activate

--- start_client.sh
+++ start_client.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 
+# cd to the directory where the server is located
+# cd /path/to/server
+
+source ./venv/bin/activate
+
 # Start the server
 echo "Starting the client (in 20 seconds)..."
 sleep 20
-# cd to the directory where the server is located
-# cd /path/to/server
+
 # Start the server
 python3 client.py
--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 
-# Start the server
-echo "Starting the server (in 20 seconds)..."
-sleep 10
 # cd to the directory where the server is located
 # cd /path/to/server
+
+source ./venv/bin/activate
+
+# Start the server
+echo "Starting the server (in 10 seconds)..."
+sleep 10
+
 # Start the server
 python3 server.py
